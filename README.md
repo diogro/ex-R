3271645161fc1a67f4fca7a6438eac29335ea3be
diogro <diogro@gmail.com>
2013-02-28 13:11:21 -0300

    Ex 3.2.2

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Problemas
 1. Ex. 1.5: o rdata criado não parece ser lido direito
 
 2. Ex. 2.5: o objetos pesos.ds é desnecessario para a conta pedida
+
+3. Ex 3.2.2: não funciona com aggregate...
---
+++ notar.3.2.2.r
@@ -0,0 +1,10 @@
+#pesos = c(0.1, 1.1, 3.7, 5.7, -1.2, -1.5, 3.0, -0.4, 0.6,
+          #1.5, -0.1, 2.0, 0.6, -3.0, -0.3, -0.2, 0.3, 1.5)
+#dieta = rep(rep(c("A", "B", "C"), each=3), 2)
+#cor = rep(c("claro", "escuro"), each=9)
+#hamsters = data.frame(dieta, cor, pesos)
+
+media = mean(hamsters$pesos)
+media.por.cor = tapply(hamsters$pesos, hamsters$cor, mean)
+media.por.dieta = tapply(hamsters$pesos, hamsters$dieta, mean)
+media.cruzada = tapply(hamsters$peso, list(hamsters$cor, hamsters$dieta), mean)
