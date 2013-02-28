d11007256f864b5b548a17b33c491d179ed41199
diogro <diogro@gmail.com>
2013-02-28 12:22:52 -0300

    Ex 2.3

---
+++ notar.2.3.r
@@ -0,0 +1,6 @@
+luz = c( 9839,10149,10486,10746,11264,11684,12082,12599,13004,13350,13717,14052)
+luz.cons = diff(luz)
+luz.m  = mean(luz.cons)
+luz.md = median(luz.cons)
+luz.v  = var(luz.cons)
+luz.range = c(min(luz.cons), max(luz.cons))
