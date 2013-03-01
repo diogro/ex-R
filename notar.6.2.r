9c87ef5510752e9eaeac243bede0f69f68f82e29
diogro <diogro@gmail.com>
2013-03-01 14:29:44 -0300

    ignore csv

--- notar.6.2.r
+++ notar.6.2.r
@@ -3,7 +3,7 @@ area.basal.ind.fuste = ((caixeta$cap/(2*pi))^2*pi)
 area.basal.ind = aggregate(area.basal.ind.fuste, list(caixeta$arvore), sum)
 area.basal.parc = aggregate(area.basal.ind.fuste, list(caixeta$parcela), sum)
 
-#dados.caixeta <- caixeta
+#dados.caixeta <- read.table("caixeta.csv", header=TRUE, sep=",")
 #AB.IND.FUST <- ((dados.caixeta$cap/(2*pi))^2*pi)
 #AB.ind <- aggregate(AB.IND.FUST,by=list(caixeta$arvore), sum)
 #AB.parc <- aggregate(AB.IND.FUST,by=list(caixeta$parcela), sum)
