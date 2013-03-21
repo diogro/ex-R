9d6eb44a4713790719afb9292bedc8c78341cf51
diogro <diogro@gmail.com>
2013-03-21 19:47:32 -0300

    data.frame names in 8.3

--- notar.8.3.r
+++ notar.8.3.r
@@ -1,6 +1,6 @@
-shannon = function(x) {return(-sum( (x[!is.na(x)]/sum(x[!is.na(x)])) * log(x[!is.na(x)]/sum(x[!is.na(x)]))))}
-simpson = function(x) {return(sum((x[!is.na(x)]/sum(x[!is.na(x)]))^2))}
-diversidade <- function(x=matrix(), indice){
-    if(indice == "shannon"){return(data.frame(apply (x, 2, shannon)))}
-    if(indice == "simpson"){return(data.frame(apply (x, 2, simpson)))}
+shannon2 = function(x) {return(-sum( (x[!is.na(x)]/sum(x[!is.na(x)])) * log(x[!is.na(x)]/sum(x[!is.na(x)]))))}
+simpson2 = function(x) {return(sum((x[!is.na(x)]/sum(x[!is.na(x)]))^2))}
+diversidade2 <- function(x, indice){
+    if(indice == "shannon"){return(data.frame(shannon = apply (x, 2, shannon2)))}
+    if(indice == "simpson"){return(data.frame(simpson = apply (x, 2, simpson2)))}
 }
