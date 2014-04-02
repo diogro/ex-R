bb989af45a03b654d2fd824f36bb9a51e083bfb7
Diogo Melo <diogro@gmail.com>
2014-04-02 18:01:58 +0100

    6.2

--- notar.6.2.r
+++ notar.6.2.r
@@ -1,10 +1,21 @@
-caixeta = read.table("http://ecologia.ib.usp.br/bie5782/lib/exe/fetch.php?media=dados:caixeta.csv", header=T, sep=',')
-area.basal.ind.fuste = ((caixeta$cap/(2*pi))^2*pi)
-area.basal.ind = aggregate(area.basal.ind.fuste, list(caixeta$arvore), sum)
-area.basal.parc = aggregate(area.basal.ind.fuste, list(caixeta$parcela), sum)
+mudas = read.table("altura-mudas.csv", header=T, sep=',')
 
-#dados.caixeta <- read.table("caixeta.csv", header=TRUE, sep=",")
-#dados.caixeta <- caixeta
-#AB.IND.FUST <- ((dados.caixeta$cap/(2*pi))^2*pi)
-#AB.ind <- aggregate(AB.IND.FUST,by=list(caixeta$arvore), sum)
-#AB.parc <- aggregate(AB.IND.FUST,by=list(caixeta$parcela), sum)
+#library(ggplot2)
+#mudas$bloco = factor(mudas$bloco)
+#mudas$substrato = factor(mudas$substrato)
+#ggplot(mudas, aes(especie, altura, group = especie)) + geom_boxplot() + facet_grid(~substrato) + theme_classic()
+
+tamboril = subset(mudas, mudas$especie == "tamboril")
+mean.tamboril = mean(tamboril$altura)
+ss.total = sum((tamboril$altura - mean.tamboril)^2)
+mean.tamboril.subs = tapply(tamboril$altura, tamboril$substrato, mean)
+ss.intra = sum((tamboril$altura - mean.tamboril.subs[tamboril$substrato])^2)
+ss.entre = 6*sum((mean.tamboril.subs - mean.tamboril)^2)
+gl = dim(tamboril)[1] - 1
+gl_entre = length(unique(tamboril$substrato)) - 1
+gl_intra = gl - gl_entre
+d_intra = ss.intra/gl_intra
+d_entre = ss.entre/gl_entre
+razao = d_entre/d_intra
+prob = pf(razao, gl_entre, gl_intra, lower.tail = FALSE)
+porc = 100* ss.intra/ss.total
