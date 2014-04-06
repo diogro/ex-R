799fcbe50683c7df1c7daf1b101668ba3bc4c0fc
Diogo Melo <diogro@gmail.com>
2014-04-06 19:14:22 +0100

    style

--- notar.7.5.r
+++ notar.7.5.r
@@ -1,17 +1,17 @@
-aves <- read.table('aves_cerrado.csv', header = T, sep=';', as.is=T)
+aves <- read.table('aves_cerrado.csv', header = T, sep = ';', as.is = T)
 aves$urubu[is.na(aves$urubu)] <- 0
 aves$carcara[is.na(aves$carcara)] <- 0
 aves$seriema[is.na(aves$seriema)] <- 0
 
 aves$fisionomia[aves$fisionomia =='ce'] = 'Ce'
 
-mod.ce  <-  lm(seriema[fisionomia=='Ce']~carcara[fisionomia=='Ce'], aves)
-mod.cc  <-  lm(seriema[fisionomia=='CC']~carcara[fisionomia=='CC'], aves)
-mod.cl  <-  lm(seriema[fisionomia=='CL']~carcara[fisionomia=='CL'], aves)
+mod.ce <- lm(seriema[fisionomia=='Ce']~carcara[fisionomia=='Ce'], aves)
+mod.cc <- lm(seriema[fisionomia=='CC']~carcara[fisionomia=='CC'], aves)
+mod.cl <- lm(seriema[fisionomia=='CL']~carcara[fisionomia=='CL'], aves)
 
-p.ce =  summary(mod.ce)$coefficients[2,4]
-p.cc =  summary(mod.cc)$coefficients[2,4]
-p.cl =  summary(mod.cl)$coefficients[2,4]
+p.ce = summary(mod.ce)$coefficients[2,4]
+p.cc = summary(mod.cc)$coefficients[2,4]
+p.cl = summary(mod.cl)$coefficients[2,4]
 
 coef.ce = coef(mod.ce)
 coef.cc = coef(mod.cc)
