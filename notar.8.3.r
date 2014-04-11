f1e001a1a698a7027975143a80250ae311bc4cb9
Diogo Melo <diogro@gmail.com>
2014-04-11 12:18:17 +0100

    8.3

--- notar.8.3.r
+++ notar.8.3.r
@@ -1,6 +1,7 @@
-shannon = function(x) {return(-sum( (x[!is.na(x)]/sum(x[!is.na(x)])) * log(x[!is.na(x)]/sum(x[!is.na(x)]))))}
-simpson = function(x) {return(sum((x[!is.na(x)]/sum(x[!is.na(x)]))^2))}
-diversidade2 <- function(x, indice){
-    if(indice == "shannon"){return(data.frame(shannon = apply (x, 2, shannon)))}
-    if(indice == "simpson"){return(data.frame(simpson = apply (x, 2, simpson)))}
+shannon = function(x) {x <- x[!is.na(x) & x != 0]; return(-sum((x/sum(x)) * log(x/sum(x))))}
+simpson = function(x) {x <- x[!is.na(x)]; return(sum((x/sum(x))^2))}
+diversidade <- function(x, indice = c("shannon", "simpson")){
+    indice = match.arg(indice)
+    if(indice == "shannon"){return(apply (x, 2, shannon))}
+    if(indice == "simpson"){return(apply (x, 2, simpson))}
 }
