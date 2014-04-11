810923c0b4bfddc0117ff387fe2ff4fe398a8538
Diogo Melo <diogro@gmail.com>
2014-04-11 12:17:56 +0100

    8.3

--- notar.8.3.r
+++ notar.8.3.r
@@ -1,6 +1,6 @@
-shannon2 = function(x) {return(-sum( (x[!is.na(x)]/sum(x[!is.na(x)])) * log(x[!is.na(x)]/sum(x[!is.na(x)]))))}
-simpson2 = function(x) {return(sum((x[!is.na(x)]/sum(x[!is.na(x)]))^2))}
+shannon = function(x) {return(-sum( (x[!is.na(x)]/sum(x[!is.na(x)])) * log(x[!is.na(x)]/sum(x[!is.na(x)]))))}
+simpson = function(x) {return(sum((x[!is.na(x)]/sum(x[!is.na(x)]))^2))}
 diversidade2 <- function(x, indice){
-    if(indice == "shannon"){return(data.frame(shannon = apply (x, 2, shannon2)))}
-    if(indice == "simpson"){return(data.frame(simpson = apply (x, 2, simpson2)))}
+    if(indice == "shannon"){return(data.frame(shannon = apply (x, 2, shannon)))}
+    if(indice == "simpson"){return(data.frame(simpson = apply (x, 2, simpson)))}
 }
