08b492deda53987c1695bed8769ca4e617d7c051
Diogo Melo <diogro@gmail.com>
2014-04-14 10:02:42 +0100

    style

--- notar.8.3.r
+++ notar.8.3.r
@@ -1,3 +1,3 @@
-shannon = function(x) {x <- x[!is.na(x) & x != 0]; return(-sum((x/sum(x)) * log(x/sum(x))))}
-simpson = function(x) {x <- x[!is.na(x)]; return(sum((x/sum(x))^2))}
+shannon <- function(x) {x <- x[!is.na(x) & x != 0]; return(-sum((x/sum(x)) * log(x/sum(x))))}
+simpson <- function(x) {x <- x[!is.na(x)]; return(sum((x/sum(x))^2))}
 diversidade <- function(x, indice = c("shannon", "simpson")){return(apply (x, 2, match.arg(indice)))}
