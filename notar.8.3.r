71febd7491cbc9de5b58f99b75fcb5abffb95dc6
Diogo Melo <diogro@gmail.com>
2014-04-14 09:50:57 +0100

    8.3 em 5 linhas? wat?!

--- notar.8.3.r
+++ notar.8.3.r
@@ -2,6 +2,4 @@ shannon = function(x) {x <- x[!is.na(x) & x != 0]; return(-sum((x/sum(x)) * log(
 simpson = function(x) {x <- x[!is.na(x)]; return(sum((x/sum(x))^2))}
 diversidade <- function(x, indice = c("shannon", "simpson")){
     indice = match.arg(indice)
-    if(indice == "shannon"){return(apply (x, 2, shannon))}
-    if(indice == "simpson"){return(apply (x, 2, simpson))}
-}
+    return(apply (x, 2, indice))}
