7e9c3d1d56e38d869340c1a764eae66bbd796d1e
Diogo Melo <diogro@gmail.com>
2022-04-11 14:49:57 -0400

    Update notar.8.3.r

--- notar.8.3.r
+++ notar.8.3.r
@@ -1,3 +1,3 @@
 shannon <- function(x) -sum(((x <- na.omit(x[x!=0]))/sum(x)) * log(x/sum(x)))
 simpson <- function(x) sum(((x <- na.omit(x))/sum(x))^2)
-diversidade <- function(x, indice = c("shannon", "simpson")){return(apply (x, 2, match.arg(indice)))}
+diversidade <- function(x, indice = c("shannon", "simpson")) apply (x, 2, match.arg(indice))
